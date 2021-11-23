96c600188003a3a7b2b0c68fa5ae16290cb7628d
Thanh Nguyen <vnscriptkid@gmail.com>
2021-11-23 22:23:07 +0700

    refactor: move App to Home

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,3 +1,34 @@
-const Home = () => <div>Home</div>;
+import { useQuery } from "@apollo/client";
+import gql from "graphql-tag";
+
+const ALL_USERS = gql`
+  query GetAllUsers {
+    allUsers {
+      id
+      username
+      email
+    }
+  }
+`;
+
+function Home() {
+  const queryInfo = useQuery(ALL_USERS);
+
+  const { data: { allUsers = [] } = {}, loading, error } = queryInfo;
+
+  if (error) return <div>oops</div>;
+
+  return (
+    <div>
+      <h1>heading</h1>
+
+      {loading ? (
+        <span>loading...</span>
+      ) : (
+        allUsers.map((user) => <li key={user.id}>{user.email}</li>)
+      )}
+    </div>
+  );
+}
 
 export default Home;
