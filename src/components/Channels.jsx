b321111cc010bc04b9f34136396acc2ddce4b444
Thanh Nguyen <vnscriptkid@gmail.com>
2021-11-26 01:42:35 +0700

    feat: better styles

--- src/components/Channels.jsx
+++ src/components/Channels.jsx
@@ -1,5 +1,11 @@
 import styled from "styled-components";
 
+const paddingLeft = "padding-left: 10px";
+
+const PushLeft = styled.div`
+  ${paddingLeft};
+`;
+
 const ChannelWrapper = styled.div`
   grid-column: 2;
   grid-row: 1 / 4;
@@ -7,27 +13,62 @@ const ChannelWrapper = styled.div`
   color: #958993;
 `;
 
-const channel = ({ id, name }) => <li key={`channel-${id}`}># {name}</li>;
+const TeamNameHeader = styled.h1`
+  color: #fff;
+  font-size: 20px;
+`;
 
-const user = ({ id, name }) => <li key={`user-${id}`}>{name}</li>;
+const SideBarList = styled.ul`
+  width: 100%;
+  list-style: none;
+  padding-left: 0px;
+`;
+
+const SideBarListHeader = styled.li`
+  ${paddingLeft};
+`;
+
+const SideBarListItem = styled.li`
+  padding: 2px;
+  ${paddingLeft};
+  &:hover {
+    background: #3e313c;
+  }
+`;
+
+const Green = styled.span`
+  color: #38978d;
+`;
+
+const Bubble = ({ on = true }) => (on ? <Green>●</Green> : "○");
+
+const channel = ({ id, name }) => (
+  <SideBarListItem key={`channel-${id}`}># {name}</SideBarListItem>
+);
+
+const user = ({ id, name }) => (
+  <SideBarListItem key={`user-${id}`}>
+    <Bubble /> {name}
+  </SideBarListItem>
+);
 
 const Channels = ({ teamName, username, channels, users }) => (
   <ChannelWrapper>
-    <div>
-      {teamName}
+    <PushLeft>
+      <TeamNameHeader>{teamName}</TeamNameHeader>
       {username}
-    </div>
+    </PushLeft>
     <div>
-      <ul>
-        <li>Channels</li>
+      <SideBarList>
+        <SideBarListHeader>Channels</SideBarListHeader>
         {channels.map(channel)}
-      </ul>
+      </SideBarList>
     </div>
     <div>
-      <ul>
-        <li>Direct Messages</li>
+      <SideBarList>
+        <SideBarListHeader>Direct Messages</SideBarListHeader>
         {users.map(user)}
-      </ul>
+      </SideBarList>
     </div>
   </ChannelWrapper>
 );
