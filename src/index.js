c109731064f0eac08db5dc47a626054d7b44596d
Thanh Nguyen <vnscriptkid@gmail.com>
2021-11-27 09:59:51 +0700

    feat: global auth handling

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ import "semantic-ui-css/semantic.min.css";
 
 import reportWebVitals from "./reportWebVitals";
 import SwitchRoutes from "./routes";
+import { get } from "lodash";
 
 const httpLink = new HttpLink({ uri: "http://localhost:4000/graphql" });
 
@@ -32,11 +33,16 @@ const populateTokens = new ApolloLink((operation, forward) => {
   return forward(operation).map((response) => {
     const context = operation.getContext();
 
-    const token = context.response.headers.get("x-token");
-    const refreshToken = context.response.headers.get("x-refresh-token");
+    if (get(response, "errors[0].message") === "Not authenticated") {
+      window.history.pushState(null, "", "login");
+      window.location.reload();
+    } else {
+      const token = context.response.headers.get("x-token");
+      const refreshToken = context.response.headers.get("x-refresh-token");
 
-    if (token) localStorage.setItem("token", token);
-    if (refreshToken) localStorage.setItem("token", refreshToken);
+      if (token) localStorage.setItem("token", token);
+      if (refreshToken) localStorage.setItem("token", refreshToken);
+    }
 
     return response;
   });
